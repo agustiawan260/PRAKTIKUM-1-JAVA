434dd6cb3e6f05d99138856de2b8d9e300aa588b
agustiawan260 <62092066+agustiawan260@users.noreply.github.com>
2020-03-12 15:12:57 +0800

    Add files via upload

---
+++ helloteman.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class helloteman{
+public static void main (String[] arg)
+{
+	System.out.println("hallo ini belajar java")
+	System.out.println("nama saya agustiawan");
+	System.out.println("nim:32019001");
+	System.out.println("ini kelas java");
+}
+}
